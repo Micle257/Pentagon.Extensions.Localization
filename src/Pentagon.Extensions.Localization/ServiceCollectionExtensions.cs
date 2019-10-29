1d7f7c3d53ee4e0512238c9cd5feb9b360d87a82
Michal Pokorný <michal.pokorny0@gmail.com>
2019-10-29 13:21:04 +0100

    DI fix

--- src/Pentagon.Extensions.Localization/ServiceCollectionExtensions.cs
+++ src/Pentagon.Extensions.Localization/ServiceCollectionExtensions.cs
@@ -19,6 +19,7 @@ namespace Pentagon.Extensions.Localization
                     .AddOptions();
 
             services.AddScoped<ICultureContext, CultureContext>();
+            services.AddScoped<ICultureContextWriter>(c => c.GetRequiredService<ICultureContext>() as CultureContext);
 
             services.Add(ServiceDescriptor.Describe(typeof(ILocalizationCache), typeof(LocalizationCache), ServiceLifetime.Transient));
             services.Add(ServiceDescriptor.Describe(typeof(ICultureManager), typeof(CultureManager), ServiceLifetime.Scoped));

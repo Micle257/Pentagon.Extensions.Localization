34e30f810667d8c46bbdd0c8bc9d74b0f03568d6
Michal Pokorny <michal.pokorny0@gmail.com>
2020-01-29 12:41:04 +0100

    add possibility to seed database with defaults from definitions

--- src/Pentagon.Extensions.Localization.EntityFramework/Persistence/ModelBuilderExtensions.cs
+++ src/Pentagon.Extensions.Localization.EntityFramework/Persistence/ModelBuilderExtensions.cs
@@ -6,6 +6,9 @@
 
 namespace Pentagon.Extensions.Localization.EntityFramework.Persistence
 {
+    using System;
+    using System.Linq;
+    using Entities;
     using Microsoft.EntityFrameworkCore;
     using Options;
 
@@ -15,6 +18,29 @@ namespace Pentagon.Extensions.Localization.EntityFramework.Persistence
         {
             builder.ApplyConfiguration(new CultureEntityConfiguration(options))
                    .ApplyConfiguration(new CultureResourceEntityConfiguration(options));
+
+            return builder;
+        }
+
+        public static ModelBuilder SeedInvariantCultureResourcesKeysFromConventions(this ModelBuilder builder, Type type = null)
+        {
+            var definitions = LocalizationDefinitionConvention.GetDefinitions(type);
+
+            builder.Entity<CultureEntity>()
+                   .HasData(new CultureEntity
+                            {
+                                    Name       = LocalizationConstants.Invariant,
+                                    ActiveFlag = true,
+                                    Id         = 1
+                            });
+
+            builder.Entity<CultureResourceEntity>()
+                   .HasData(definitions.Select(a => new CultureResourceEntity
+                                                    {
+                                                            CultureId = 1,
+                                                            Key = a.Key,
+                                                            Value = a.Key
+                                                    }));
 
             return builder;
         }
